279a4c2ce8ee49e5d3b992974f12c1a9f6d26e8c
Giorgio Morales Luna <giorgiomoralesluna@gmail.com>
2025-03-04 20:16:25 +0100

    Re-structured project

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 from PREnsemble.Data.DataLoader import DataLoader
 from PREnsemble.EvaluateModel import EvaluateModel
 
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
 if __name__ == '__main__':
     data = DataLoader(name='grT')
     emodel = EvaluateModel(dataset=data.dataset, complexity=2)
