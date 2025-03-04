ae19a115872862069df4a273726b82fbdaee0100
Giorgio Morales Luna <giorgiomoralesluna@gmail.com>
2025-03-04 20:16:54 +0100

    Re-structured project

--- Main.py
+++ Main.py
@@ -1,7 +1,11 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
 from PREnsemble.Data.DataLoader import DataLoader
 from PREnsemble.EvaluateModel import EvaluateModel
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 if __name__ == '__main__':
     data = DataLoader(name='grT')
